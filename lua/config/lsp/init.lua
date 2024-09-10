5627f967a7b0873187d9b72de05fa5d407a43fb2
Todd W. Fulton <todd.w.fulton@gmail.com>
2024-09-10 06:47:07 -0500

    Move lsp configs into config.lsp

--- lua/config/lsp/init.lua
+++ lua/config/lsp/init.lua
@@ -1,1 +1,11 @@
-return {}
+local modpath = (...)
+local M = {}
+
+for filename in io.popen('ls -pUqAL '.. modpath):lines() do
+   filename = filename:match"^(.*)%.lua$"
+   if filename and filename ~= "init" then
+      M[filename] = require(modpath .. "." .. filename)
+   end
+end
+
+return M
--- lua/lsp-configs/init.lua
+++
@@ -1,6 +0,0 @@
-return {
-  asm_lsp = require'lsp-configs.asm',
-  clangd = require'lsp-configs.clangd',
-  cmake = require'lsp-configs.cmake',
-  lua_ls = require'lsp-configs.lua',
-}
