c5fc699d72036e67d0abfa3251d11758fc84321a
Todd W. Fulton <todd.w.fulton@gmail.com>
2024-09-10 07:02:41 -0500

    start a utils module

---
+++ lua/utils/init.lua
@@ -0,0 +1,23 @@
+local M = {}
+
+local function is_win()
+  return package.config:sub(1, 1) == '\\'
+end
+
+local function get_path_separator()
+  if is_win() then
+    return '\\'
+  end
+  return '/'
+end
+
+
+function M.script_path()
+  local str = debug.getinfo(2, 'S').source:sub(2)                  
+  if is_win() then                                                 
+    str = str:gsub('/', '\\')                                      
+  end                                                              
+  return str:match('(.*' .. get_path_separator() .. ')')           
+end
+
+return M
