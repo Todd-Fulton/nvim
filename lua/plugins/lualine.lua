916281460c424b771ccb54d3974a09aef3c522e1
Todd W. Fulton <todd.w.fulton@gmail.com>
2024-09-10 18:47:36 -0500

    Tried out lsp-progress.nvim (disable for now, we already get notify)

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,15 +1,18 @@
 return {
   'nvim-lualine/lualine.nvim',
-  dependencies = { 'nvim-tree/nvim-web-devicons' },
-  config = function() 
+  dependencies = {
+    'nvim-tree/nvim-web-devicons',
+    -- { "linrongbin16/lsp-progress.nvim", opts = {} },
+  },
+  opts = function(_, opts)
     local lazy_status = require('lazy.status')
-
-    opts = {
+    opts = opts or {}
+    opts = vim.tbl_deep_extend('force', opts, {
       options = {
         icons_enabled = true,
         theme = 'auto',
-        component_separators = { left = '', right = ''},
-        section_separators = { left = '', right = ''},
+        component_separators = { left = '', right = '' },
+        section_separators = { left = '', right = '' },
         disabled_filetypes = {
           statusline = {
             -- "neo-tree",
@@ -26,30 +29,30 @@ return {
         }
       },
       sections = {
-        lualine_a = {'mode'},
-        lualine_b = {'branch', 'diff', 'diagnostics'},
-        lualine_c = {'filename'},
+        lualine_a = { 'mode' },
+        lualine_b = { 'branch', 'diff', 'diagnostics' },
+        lualine_c = { function () return require'lsp-progress'.progress() end },
         lualine_x = {
-          { 
-            lazy_status.updates,
-            cond = lazy_status.has_updates,
-            color = { fg = "#FF9E64" },
-          },
+          -- {
+          --   lazy_status.updates,
+          --   cond = lazy_status.has_updates,
+          --   color = { fg = "#FF9E64" },
+          -- },
           { 'encoding' },
           { 'fileformat' },
           { 'filetype' },
         },
-        lualine_y = {'progress'},
-        lualine_z = {'location'}
+        lualine_y = { 'progress' },
+        lualine_z = { 'location' }
       },
-      inactive_sections = {
-        lualine_a = {},
-        lualine_b = {},
-        lualine_c = {'filename'},
-        lualine_x = {'location'},
-        lualine_y = {},
-        lualine_z = {}
-      },
+      -- inactive_sections = {
+      --   lualine_a = {},
+      --   lualine_b = {},
+      --   lualine_c = { 'filename' },
+      --   lualine_x = { 'location' },
+      --   lualine_y = {},
+      --   lualine_z = {}
+      -- },
       tabline = {},
       winbar = {},
       inactive_winbar = {},
@@ -57,7 +60,14 @@ return {
         "neo-tree",
         "lazy",
       },
-    }
-    require'lualine'.setup(opts)
+    })
+    -- listen lsp-progress event and refresh lualine
+    -- vim.api.nvim_create_augroup("lualine_augroup", { clear = true })
+    -- vim.api.nvim_create_autocmd("User", {
+    --   group = "lualine_augroup",
+    --   pattern = "LspProgressStatusUpdated",
+    --   callback = require("lualine").refresh,
+    -- })
+    return opts
   end,
 }
