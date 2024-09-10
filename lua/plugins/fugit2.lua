46e3b500b560788fb391059288b6474829ff8995
Todd W. Fulton <todd.w.fulton@gmail.com>
2024-09-09 21:20:46 -0500

    Use diffview.nvim for now (stability)

--- lua/plugins/fugit2.lua
+++ lua/plugins/fugit2.lua
@@ -1,19 +1,28 @@
-return {
-  'SuperBo/fugit2.nvim',
-  opts = {
-    width = 150,
-  },
-  dependencies = {
-    'MunifTanjim/nui.nvim',
-    'nvim-tree/nvim-web-devicons',
-    'nvim-lua/plenary.nvim',
-    {
-      'chrisgrieser/nvim-tinygit', -- optional: for Github PR view
-      dependencies = { 'stevearc/dressing.nvim' }
+return  {
+  {
+    'SuperBo/fugit2.nvim',
+    opts = {
+      width = 150,
+      external_diffview = true, -- use diffview.nvim until fugit2's is more stable
     },
+    dependencies = {
+      'MunifTanjim/nui.nvim',
+      'nvim-tree/nvim-web-devicons',
+      'nvim-lua/plenary.nvim',
+      {
+        'chrisgrieser/nvim-tinygit', -- optional: for Github PR view
+        dependencies = { 'stevearc/dressing.nvim' }
+      },
+      'sindrets/diffview.nvim'
+    },
+    cmd = { 'Fugit2', 'Fugit2Blame', 'Fugit2Diff', 'Fugit2Graph' },
   },
-  cmd = { 'Fugit2', 'Fugit2Diff', 'Fugit2Graph' },
-  keys = {
-    { '<leader>F', mode = 'n', '<cmd>Fugit2<cr>' }
+  {
+    'sindrets/diffview.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    -- lazy, only load diffview by these commands
+    cmd = {
+      'DiffviewFileHistory', 'DiffviewOpen', 'DiffviewToggleFiles', 'DiffviewFocusFiles', 'DiffviewRefresh'
+    }
   }
 }
