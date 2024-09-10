635b528c99d0fc924f65a04fd39e2d967163b241
Todd W. Fulton <todd.w.fulton@gmail.com>
2024-09-10 07:03:13 -0500

    fix up lsp and dap config, modpath was not the droid I was looking for.

--- lua/config/dap/adapters/init.lua
+++ lua/config/dap/adapters/init.lua
@@ -1,8 +1,9 @@
 local modpath = (...)
-
 local M = {}
 
-for filename in io.popen('ls -pUqAL '.. modpath):lines() do
+local utils = require'utils'
+
+for filename in io.popen('ls -pUqAL '.. utils.script_path()):lines() do
    filename = filename:match"^(.*)%.lua$"
    if filename and filename ~= "init" then
       M[filename] = require(modpath .. "." .. filename)
