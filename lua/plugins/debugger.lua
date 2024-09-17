efbec61324241e42ad96e679cb96db804d189fc1
Todd W. Fulton <todd.w.fulton@gmail.com>
2024-09-16 23:41:43 -0500

    install dap-ui and make debugging plugins more lazy

--- lua/plugins/debugger.lua
+++ lua/plugins/debugger.lua
@@ -1,15 +1,26 @@
 return {
   {
     "mfussenegger/nvim-dap",
+    ft = {"cpp", "c", "lua", "asm" },
     config = function()
-      local dap = require'dap'
-      local dap_configs = require'user.configs.dap.init'
+      local dap = require 'dap'
+      local dap_configs = require 'user.configs.dap.init'
       for adapter, map in pairs(dap_configs.adapters) do
         dap[adapter] = map
       end
       for config, map in pairs(dap_configs.configurations) do
         dap[config] = map
       end
+      require ('dapui').setup()
     end,
   },
+  {
+    "rcarriga/nvim-dap-ui",
+    dependencies = {
+      "mfussenegger/nvim-dap",
+      "nvim-neotest/nvim-nio"
+    },
+    lazy = true,
+    opts = {},
+  }
 }
