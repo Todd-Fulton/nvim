4ac4e17c817bc87b6b1bd05e34f5be4f4614004c
Todd W. Fulton <todd.w.fulton@gmail.com>
2024-09-17 21:23:10 -0500

    Add workspace keybindings

--- lua/plugins/workspaces.lua
+++ lua/plugins/workspaces.lua
@@ -11,6 +11,10 @@ return {
     "WorkspacesRename",
     "WorkspacesSyncDirs",
   },
+  keys = {
+    { "<Leader>fw", "<CMD>Telescope workspaces<CR>", desc = "Workspaces" },
+    { "<Leader>Sw", "<CMD>WorkspacesAdd<CR>",        desc = "Workspace add" }
+  },
   opts = {
     cd_type = "tab"
   },
