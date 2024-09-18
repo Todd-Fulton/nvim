5b245642bf8da33871aef1e4c9d5676ec5399d44
Todd W. Fulton <todd.w.fulton@gmail.com>
2024-09-17 21:24:30 -0500

    Add cmake filetype to load cmake-tools

--- lua/plugins/coding/cmake-tools.lua
+++ lua/plugins/coding/cmake-tools.lua
@@ -1,6 +1,7 @@
+---TODO: Load when opening a workspace/project configured with cmake
 return {
   "Civitasv/cmake-tools.nvim",
-  ft = { "c", "cpp", "objc", "objcpp", "cuda", "proto" },
+  ft = { "c", "cpp", "objc", "objcpp", "cuda", "proto", "cmake" },
   dependencies = {
     'stevearc/overseer.nvim',
   },
