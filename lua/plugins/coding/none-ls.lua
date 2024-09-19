3442b12c3c3358e82422fa8e9b3dd532bf59bb76
Todd W. Fulton <todd.w.fulton@gmail.com>
2024-09-19 10:12:19 -0500

    Remove gitsigns from sources, it's too noisy with code actions.

--- lua/plugins/coding/none-ls.lua
+++ lua/plugins/coding/none-ls.lua
@@ -2,14 +2,12 @@ return {
   "nvimtools/none-ls.nvim",
   dependencies = {
     "nvim-lua/plenary.nvim",
-    "lewis6991/gitsigns.nvim",
   },
   ft = {"lua"},
   config = function()
     local null_ls = require("null-ls")
     null_ls.setup({
       sources = {
-        null_ls.builtins.code_actions.gitsigns,
         null_ls.builtins.diagnostics.selene,
       }
     })
