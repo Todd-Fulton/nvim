b14d63faf7413f9a09b8aacd9b743bb0dacc0b30
Todd W. Fulton <todd.w.fulton@gmail.com>
2024-09-20 12:48:48 -0500

    format file

--- lua/plugins/coding/todo-comments.lua
+++ lua/plugins/coding/todo-comments.lua
@@ -6,13 +6,13 @@ return {
   init = function()
     require'user.configs.keymaps.init'.add({
       mode = { "n" },
-      { 
+      {
         "]t",
         function() require'todo-comments'.jump_next() end,
         desc = "Next todo",
         --- TODO: add condition, only activate if we are in a buffer
       },
-      { 
+      {
         "[t",
         function() require'todo-comments'.jump_prev() end,
         desc = "Prev todo",
