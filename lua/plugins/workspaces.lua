bc8498bb266a806a3fed14b62dc3041bcd3a1bc1
Todd W. Fulton <todd.w.fulton@gmail.com>
2024-09-20 13:41:42 -0500

    Add keybinding to open a workspace in a new tab

--- lua/plugins/workspaces.lua
+++ lua/plugins/workspaces.lua
@@ -13,7 +13,13 @@ return {
   },
   keys = {
     { "<Leader>fw", "<CMD>Telescope workspaces<CR>", desc = "Workspaces" },
-    { "<Leader>Sw", "<CMD>WorkspacesAdd<CR>",        desc = "Workspace add" }
+    { "<Leader>Sw", "<CMD>WorkspacesAdd<CR>",        desc = "Workspace add" },
+    { "<Leader>tw", function ()
+      vim.cmd [[:$tabnew]]
+      require("workspaces").open()
+    end,
+    desc = "Open Workspace in a new tab",
+    },
   },
   opts = {
     cd_type = "tab"
