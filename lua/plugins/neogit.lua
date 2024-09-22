f2ca0b347c780c00c723badd872935d07c1b5456
Todd W. Fulton <todd.w.fulton@gmail.com>
2024-09-21 22:57:10 -0500

    Add key bindings with icons

--- lua/plugins/neogit.lua
+++ lua/plugins/neogit.lua
@@ -1,17 +1,25 @@
 return {
   "NeogitOrg/neogit",
   dependencies = {
-    "nvim-lua/plenary.nvim",         -- required
-    "sindrets/diffview.nvim",        -- optional - Diff integration
+    "nvim-lua/plenary.nvim", -- required
+    "sindrets/diffview.nvim", -- optional - Diff integration
 
     "nvim-telescope/telescope.nvim", -- optional
   },
   cmd = "Neogit",
-  keys = {{"<Leader>gv", "<CMD>Neogit<CR>", desc = "Open Neogit" }},
   opts = {
     graph_stype = "unicode",
     commit_editor = {
       kind = "auto",
     },
   },
+  init = function()
+    require("which-key").add {
+      { "<Leader>gnt", "<CMD>Neogit<CR>", desc = "Open Neogit" },
+      { "<Leader>gnc", "CMD>Neogit kind=tab commit<CR>", icon = "", desc = "Commit staged files/hunks" },
+      { "<Leader>gnd", "CMD>Neogit diff<CR>", icon = "", desc = "Commit staged files/hunks" },
+      { "<Leader>gnP", "CMD>Neogit push<CR>", icon = "", desc = "Commit staged files/hunks" },
+      { "<Leader>gnp", "CMD>Neogit pull<CR>", icon = "", desc = "Commit staged files/hunks" },
+    }
+  end,
 }
