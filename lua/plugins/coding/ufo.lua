c262f3820ad6477e8b3854662b855964466a7cf6
Todd W. Fulton <todd.w.fulton@gmail.com>
2024-09-27 11:29:10 -0500

    [Fix] Monkey patch ufo.model.buffer.attach

--- lua/plugins/coding/ufo.lua
+++ lua/plugins/coding/ufo.lua
@@ -1,8 +1,16 @@
-local ft_ignore = {
-  "neo-tree",
-  "NeogitStatus",
-  "NeogitCommitMessage",
-  "alpha",
+local ft_foldable = {
+  "c",
+  "cpp",
+  "lua",
+  "rust",
+  "python",
+  "markdown",
+  "javascript",
+  "typescript",
+  "asm",
+  "html",
+  "css",
+  "cmake",
 }
 
 return {
@@ -33,45 +41,42 @@ return {
     event = "BufReadPost",
     opts = {},
     config = function(_, opts)
-      vim.api.nvim_create_autocmd("FileType", {
-        callback = function(ev)
-          -- TODO:
-          --   NeogitDiffView should have folds and linenumbers, but no signs column, need
-          --   to specifiy that specifically
-
-          if vim.fn.index(ft_ignore, vim.bo.filetype) ~= -1 then
-            require("ufo").detach()
-            vim.api.nvim_set_option_value("foldenable", false, { scope = "local" })
-            vim.api.nvim_set_option_value("foldcolumn", "0", { scope = "local" })
-            vim.api.nvim_set_option_value("statuscolumn", "", {
-              scope = "local",
-              buffer = ev.bufm,
-            })
-          else
-            vim.api.nvim_set_option_value("foldenable", true, { scope = "local" })
-            vim.api.nvim_set_option_value("foldlevel", 99, { scope = "local" })
-            vim.api.nvim_set_option_value("foldlevelstart", 99, { scope = "local" })
-            vim.api.nvim_set_option_value("foldcolumn", "1", { scope = "local" })
-            vim.api.nvim_set_option_value(
-              "fillchars",
-              [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]],
-              { scope = "local" }
-            )
-            vim.api.nvim_set_option_value("statuscolumn", "%!v:lua.require('statuscol').get_statuscol_string()", {
-              scope = "local",
-              buffer = ev.bufm,
-            })
-            vim.keymap.set("n", "zR", function() require("ufo").openAllFolds() end, {
-              buffer = ev.buf,
-            })
-            vim.keymap.set("n", "zM", function() require("ufo").closeAllFolds() end, {
-              buffer = ev.buf,
-            })
-          end
-        end,
-      })
+      -- monkey-patch ufo.model.buffer.attach
+      local old_attach = require("ufo.model.buffer").attach
+      ---@diagnostic disable-next-line: duplicate-set-field
+      require("ufo.model.buffer").attach = function(self)
+        if vim.fn.index(ft_foldable, vim.bo.filetype) ~= -1 then
+          vim.api.nvim_set_option_value("foldenable", true, { scope = "local" })
+          vim.api.nvim_set_option_value("foldlevel", 99, { scope = "local" })
+          vim.api.nvim_set_option_value("foldlevelstart", 99, { scope = "local" })
+          vim.api.nvim_set_option_value("foldcolumn", "1", { scope = "local" })
+          vim.api.nvim_set_option_value(
+            "fillchars",
+            [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]],
+            { scope = "local" }
+          )
+          vim.api.nvim_set_option_value("statuscolumn", "%!v:lua.require('statuscol').get_statuscol_string()", {
+            scope = "local",
+          })
+          vim.keymap.set("n", "zR", function() require("ufo").openAllFolds() end, {
+            buffer = self.bufnr,
+          })
+          vim.keymap.set("n", "zM", function() require("ufo").closeAllFolds() end, {
+            buffer = self.bufnr,
+          })
+          return old_attach(self)
+        else
+          vim.api.nvim_set_option_value("foldenable", false, { scope = "local" })
+          vim.api.nvim_set_option_value("foldcolumn", "0", { scope = "local" })
+          vim.api.nvim_set_option_value("statuscolumn", "", {
+            scope = "local",
+          })
+          return false
+        end
+      end
       -- UFO folding
-      require("ufo").setup(opts)
+      opts = opts or {}
+      require("ufo").setup(vim.tbl_extend("keep", opts, {}))
     end,
   },
 }
